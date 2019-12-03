e6ff263b8f425ca217104c7de69e5c2e5dc2405a
mizu0327 <yg0327955@gmail.com>
2019-12-03 17:24:17 +0900

    Setup asynchronous-communication

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,61 @@
+$(function(){
+  function buildHTML(message) {
+    console.log(message);
+    if (message.image) {
+      var html = `<div class="main_chat__messages__message">
+                    <div class="main_chat__messages__message__upper-info">
+                      <div class="main_chat__messages__message__upper-info__talker">
+                        ${message.user_name}
+                        </div>
+                      <div class="main_chat__messages__message__upper-info__date">
+                        ${message.date}
+                        </div>
+                      </div>
+                    <div class="main_chat__messages__message__text">
+                      ${message.body}
+                      </div>
+                      <img src=${message.image} >
+                    </div>`
+    } else {
+      var html = `<div class="main_chat__messages__message">
+                    <div class="main_chat__messages__message__upper-info">
+                      <div class="main_chat__messages__message__upper-info__talker">
+                        ${message.user_name}
+                        </div>
+                      <div class="main_chat__messages__message__upper-info__date">
+                        ${message.date}
+                        </div>
+                      </div>
+                    <div class="main_chat__messages__message__text">
+                      ${message.body}
+                      </div>
+                    </div>`
+    }
+    return html;
+  };
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault()
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.main_chat__messages').append(html);
+      $('.main_chat__messages').animate({ scrollTop: $('.main_chat__messages')[0].scrollHeight});
+      $('form')[0].reset();
+    })
+    .fail(function(){
+      alert('error');
+    });
+    return false;
+  });
+});
+
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,5 @@
+json.id @message.id
+json.user_name @message.user.name
+json.date @message.created_at
+json.body @message.body
+json.image @message.image
